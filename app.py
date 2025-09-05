4d003acf4c0b9a5fa48c9d73a71a078d692cbe5f
Flixbus3141 <felix.glab@gmail.com>
2025-09-05 10:33:06 -1000

    0.2

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
+import logging
 
 app = Flask(__name__)
 DATEIPFAD = 'activities.txt'
-
+app.debug = True
+app.logger.setLevel(logging.DEBUG)
 
 def lade_activity():
     activity = []
     try:
         with open(DATEIPFAD, 'r', encoding='utf-8') as f:
             for zeile in f:
+                app.logger.debug(zeile)
                 name, ort = zeile.strip().split(',')
                 activity.append({'name': name, 'ort': ort})
+                app.logger.debug(activity)
     except FileNotFoundError:
         open(DATEIPFAD, 'w').close()  # Lege die Datei an, wenn sie fehlt
     return activity
@@ -19,13 +23,13 @@ def lade_activity():
 def speichere_activity(activity):
     with open(DATEIPFAD, 'w', encoding='utf-8') as f:
         for act in activity:
-            f.write(f"{act['name']},{act['rasse']}\n")
+            f.write(f"{act['name']},{act['ort']}\n")
 
 
 @app.route('/')
 def index():
     activity= lade_activity()
-    return render_template('index.html', activity=activity)
+    return render_template('home.html', activity=activity)
 
 
 @app.route('/neu', methods=['GET', 'POST'])
@@ -34,34 +38,43 @@ def neu():
         name = request.form['name']
         ort = request.form['ort']
         activity = lade_activity()
-        tiere.append({'name': name, 'ort': ort})
+        activity.append({'name': name, 'ort': ort})
         speichere_activity(activity)
-        return redirect(url_for('home'))
+        return redirect(url_for('overview'))
     return render_template('create.html')
 
 
 @app.route('/bearbeiten/<name>', methods=['GET', 'POST'])
 def bearbeiten(name):
-    tiere = lade_tiere()
-    tier = next((t for t in tiere if t['name'] == name), None)
-    if not tier:
-        return "Tier nicht gefunden", 404
+    activity = lade_activity()
+    act = next((t for t in activity if t['name'] == name), None)
+    if not act:
+        return "Activität nicht gefunden", 404
 
     if request.method == 'POST':
-        tier['name'] = request.form['name']
-        tier['rasse'] = request.form['rasse']
-        speichere_tiere(tiere)
-        return redirect(url_for('index'))
+        act['name'] = request.form['name']
+        act['ort'] = request.form['ort']
+        speichere_activity(activity)
+        return redirect(url_for('overview'))
 
-    return render_template('edit.html', tier=tier)
+    return render_template('edit.html', act=act)
 
 
 @app.route('/loeschen/<name>', methods=['POST'])
 def loeschen(name):
-    tiere = lade_tiere()
-    tiere = [t for t in tiere if t['name'] != name]
-    speichere_tiere(tiere)
-    return redirect(url_for('index'))
+    activity = lade_activity()
+    activity = [t for t in activity if t['name'] != name]
+    speichere_activity(activity)
+    return redirect(url_for('overview'))
+
+@app.route('/overview')
+def overview():
+    activity = lade_activity()
+    return render_template('overview.html',activity = activity)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
